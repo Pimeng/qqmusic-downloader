00bb9798cba28895cbf749f8a879c776a2b35619
Pimeng <pimeng@vip.qq.com>
2026-05-12 22:41:40 +0800

    fix

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;500;700;900&family=Orbitron:wght@400;500;700;900&display=swap" rel="stylesheet" />
+        <script defer src="https://u.a.07210700.xyz/script.js" data-website-id="126a89f9-d7d4-4e24-96a2-9eaf05c1838e"></script>
       </head>
       <body>
         <AppProvider>
